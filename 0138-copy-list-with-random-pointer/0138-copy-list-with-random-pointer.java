2e602785302c69c00107786d16f42bf93b307657
Harshdeep kaur <72741127+Harshdeep-k01@users.noreply.github.com>
2024-08-27 22:04:13 +0530

    Time: 0 ms (100.00%), Space: 45 MB (7.06%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,17 +15,45 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null)
-      return null;
-    if (map.containsKey(head))
-      return map.get(head);
+        Node temp = head;
+        // Create a HashMap to map original nodes
+        // to their corresponding copied nodes
+        HashMap<Node, Node> map = new HashMap<>();
 
-    Node newNode = new Node(head.val);
-    map.put(head, newNode);
-    newNode.next = copyRandomList(head.next);
-    newNode.random = copyRandomList(head.random);
-    return newNode;
-  }
+        // Step 1: Create copies of each
+        // node and store them in the map
+        while (temp != null) {
+            // Create a new node with the
+            // same data as the original node
+            Node newNode = new Node(temp.val);
+            // Map the original node to its
+            // corresponding copied node in the map
+            map.put(temp, newNode);
+            // Move to the next node in the original list
+            temp = temp.next;
+        }
 
-  private Map<Node, Node> map = new HashMap<>();
+        temp = head;
+        // Step 2: Connect the next and random
+        // pointers of the copied nodes using the map
+        while (temp != null) {
+            // Access the copied node corresponding
+            // to the current original node
+            Node copyNode = map.get(temp);
+            // Set the next pointer of the copied node
+            // to the copied node mapped to the
+            // next node in the original list
+            copyNode.next = map.get(temp.next);
+            // Set the random pointer of the copied node
+            // to the copied node mapped to the
+            // random node in the original list
+            copyNode.random = map.get(temp.random);
+            // Move to the next node in the original list
+            temp = temp.next;
+        }
+
+        // Return the head of the
+        // deep copied list from the map
+        return map.get(head);
     }
+}
